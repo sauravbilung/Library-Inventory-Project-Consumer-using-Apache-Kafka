9b69dc0a0bd1bab84c4dc2a0c5de708e049c573c
sauravbilung <saurav.bilung@gmail.com>
2021-05-29 16:32:46 +0530

    Initial commit

---
+++ src/main/java/com/learningkafka/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,18 @@
+package com.learningkafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer,String> consumerRecord){
+
+        // Kafka listener container is going to poll the records and it is going
+        // to get multiple records at the same time, but it is going to pass the
+        // records one by one to this onMessage method.
+        System.out.println("ConsumerRecord : "+consumerRecord);
+    }
+}
