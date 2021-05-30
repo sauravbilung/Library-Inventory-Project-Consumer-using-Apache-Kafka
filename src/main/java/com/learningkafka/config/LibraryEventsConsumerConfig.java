e702a731b611cbcec93c76c70a79a684c23a305a
sauravbilung <saurav.bilung@gmail.com>
2021-05-30 16:41:45 +0530

    Consumer offsets

--- src/main/java/com/learningkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learningkafka/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,27 @@
 package com.learningkafka.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+// Here are modifying the AckMode from default mode which is BATCH to MANUAL.
+// For the below logic to work we need to have a listener of certain type.
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
 
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+                ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+                configurer.configure(factory, kafkaConsumerFactory);
+                factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
 }
--- src/main/java/com/learningkafka/consumer/LibraryEventsConsumer.java
+++ src/main/java/com/learningkafka/consumer/LibraryEventsConsumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+// If @Component is commented it means that LibraryEventsConsumerManualOffest is active.
 @Component
 public class LibraryEventsConsumer {
 
