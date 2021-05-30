0c032d0804c87472d14149447d920fb281f6f32b
sauravbilung <saurav.bilung@gmail.com>
2021-05-30 17:36:35 +0530

    Concurrent consumers

--- src/main/java/com/learningkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learningkafka/config/LibraryEventsConsumerConfig.java
@@ -8,8 +8,13 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 
-// Here are modifying the AckMode from default mode which is BATCH to MANUAL.
+
+// 1) Here are modifying the AckMode from default mode which is BATCH to MANUAL.
 // For the below logic to work we need to have a listener of certain type.
+
+// 2) @KafkaListener containers in LibraryEventsConsumer classes uses concurrent message listener containers.
+// With concurrent message listener container, we can spin up multiple instances of the same kafka message
+// listener container. This approach is recommended when the application is not running in a cloud like environment.
 
 @Configuration
 @EnableKafka
@@ -21,7 +26,8 @@ public class LibraryEventsConsumerConfig {
             ConsumerFactory<Object, Object> kafkaConsumerFactory) {
                 ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
                 configurer.configure(factory, kafkaConsumerFactory);
-                factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+                factory.setConcurrency(3); // Point 2. Creating 3 threads of same with same instance of kakfa listener
+                //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // Point No.1
         return factory;
     }
 }
